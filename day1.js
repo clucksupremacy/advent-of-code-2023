a44e14fabef18bfafcaa7c88f8e9888be448f390
clucksupremacy <cherrycluckcluck@gmail.com>
2024-01-03 19:38:43 -0800

    brainstorm for part 2

--- day1.js
+++ day1.js
@@ -35,7 +35,47 @@ function first_last_numbers(line) {
     return new_number;
 }
 
-// string_in_text= "19qdlpmdrxone7sevennine";
-// substring_one = "one";
-// index = string_in_text.indexOf(substring_one); 
-// console.log(index); 
+// let string_in_text = line;
+// let substring_num = 
+//     ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+// let index_substring = string_in_text.indexOf(substring_num); 
+// // console.log(index_substring);
+// let substring_found = 
+//     string_in_text.substring(index_substring, index_substring + substring_num.length);
+// //now convert into a number
+// switch (substring_found) {
+//     case "one": 
+//         substring_found = 1;
+//         break; 
+//     case "two": 
+//         substring_found = 2;
+//         break; 
+//     case "three": 
+//         substring_found = 3;
+//         break; 
+//     case "four": 
+//         substring_found = 4;
+//         break; 
+//     case "five": 
+//         substring_found = 5;
+//         break; 
+//     case "six": 
+//         substring_found = 6;
+//         break; 
+//     case "seven": 
+//         substring_found = 7;
+//         break; 
+//     case "eight": 
+//         substring_found = 8;
+//         break; 
+//     case "nine": 
+//         substring_found = 9; 
+// }
+
+
+//take a line of string 
+//see if the string contains either a digit or any of the words in an array 
+    //have to convert the words into numbers first. 
+//if it does, return the first number that appears. store it in a variable.
+//keep going through the string until you reach the last number. store that in a different variable. 
+//if it doesn't, nothing happens 
