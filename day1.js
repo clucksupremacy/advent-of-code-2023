ff8de5f5d651a1b31f5079a000c25c26db8b7b1d
clucksupremacy <cherrycluckcluck@gmail.com>
2024-01-30 20:14:37 -0800

    final answer

--- day1.js
+++ day1.js
@@ -1,73 +1,63 @@
-// var fs = require("fs");
-// var text = fs.readFileSync("day1input.txt");
-// var text_to_string = text.toString();
-// var text_by_line = text_to_string.split("\n")
-// // console.log(text_by_line);
+var fs = require("fs");
+var text = fs.readFileSync("day1input.txt");
+var text_to_string = text.toString();
+var text_by_line = text_to_string.split("\n")
+// console.log(text_by_line);
 
-// let sum = 0;
-// for (let i = 0; i < text_by_line.length; i++) {
-//     let line = text_by_line[i];
-//     line = convert_to_number(line);
-//     combined_num = first_last_numbers(line);
-//     sum += combined_num;
-//     console.log(sum);
-//     // console.log(line);
-// }
+let sum = 0;
+for (let i = 0; i < text_by_line.length; i++) {
+    let line = text_by_line[i];
+    line = convert_to_number(line);
+    combined_num = first_last_numbers(line);
+    sum += combined_num;
+    console.log(sum);
+    // console.log(line);
+}
 
-// function first_last_numbers(line) {
-//     let first = true;
-//     let first_digit = 0;
-//     let last_digit = 0; 
-//     for (let i = 0; i < line.length; i++) {
-//         let char = line[i];
-//         if (char >= '0' && char <= '9') {
-//             if (first) {
-//                 first = false;
-//                 first_digit = char;
-//             }
-//             last_digit = char;
-//         }
-//         // console.log(char);
-//     }
-//     let both_digits = first_digit + last_digit;
-//     new_number = Number(both_digits);
+function first_last_numbers(line) {
+    let first = true;
+    let first_digit = 0;
+    let last_digit = 0; 
+    for (let i = 0; i < line.length; i++) {
+        let char = line[i];
+        if (char >= '0' && char <= '9') {
+            if (first) {
+                first = false;
+                first_digit = char;
+            }
+            last_digit = char;
+        }
+        // console.log(char);
+    }
+    let both_digits = first_digit + last_digit;
+    new_number = Number(both_digits);
 
-//     // console.log(first_digit);
-//     // console.log(new_number);
-//     return new_number;
-// }
+    // console.log(first_digit);
+    // console.log(new_number);
+    return new_number;
+}
 
-// function convert_to_number(line) {
-    let line = "xtwone3four";
+function convert_to_number(line) {
+    // let line = "xtwone3four";
     // let line = "joneseven2sseven64chvczzn";
+    // let line = "five4tmeightkfdkhdfq34eight";
     let substring_num = 
         ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-
-    let substring_num_2 = [...substring_num];
-    let subs_index = [];
-    let a = 0;
-
-    //while a number word is present in string, expand substring_num_2 to include repeats. Reorder substring_num_2 in ascending order. Then match subs_index to reflect that. 
-    //I don't think this is the best way, but it's one way to do it. 
-
-    for (i = 0; i < substring_num.length; i++) {
-        subs_index.push(line.indexOf(substring_num[i])); 
-    } 
-    //console.log(subs_index);
 
     String.prototype.insertCharacter = function(index, replacement) {
             return this.substring(0, index) + replacement + this.substring(index + replacement.length-1); 
     }
 
-    for (let i = 0; i < substring_num_2.length; i++) {
-        let index_in_string = line.indexOf(substring_num_2[i]);
-        // console.log(index_in_string);
-        if (index_in_string == -1) {
-            // console.log("failed")
-        } else {
-            line = line.insertCharacter(index_in_string+(substring_num_2[i].length-1), (subs_index.indexOf(index_in_string)+1).toString());
+    for (let i = 0; i < substring_num.length; i++) {
+        let index_in_string = line.indexOf(substring_num[i]);
+        while (index_in_string != -1) {
+            line = line.insertCharacter(index_in_string+(substring_num[i].length-1), (i+1).toString());
             console.log(line); 
+            index_in_string = line.indexOf(substring_num[i])
         }
+        
+        //let index_in_string = line.indexOf(substring_num[i]);
+        
     }
-//     return line;
-// }
+    return line;
+}
