91809f17a59edb71553de1d58c7d2274991e3b1c
clucksupremacy <cherrycluckcluck@gmail.com>
2025-01-22 18:57:18 -0800

    delete extraneous elements from main array

--- day2.js
+++ day2.js
@@ -25,14 +25,14 @@ for (let i = 0; i < text_by_line.length; i++) {
 // console.log(arr_games);
 
 //delete element from array arr_games if string contains a number greater than 14
-    //first, store number and color as individual elements in an array
+    //first, separate number and color as individual elements in an array
 let arr_split = [];
 for (let i = 0; i < arr_games.length; i++) {
     let str = arr_games[i].value;
-    let nums = str.split(' ');
+    let elem = str.split(' ');
     arr_split.push({
                     id: i+1,
-                    value: nums
+                    value: elem
                 });
 }
     //second, convert the number-strings in the array to number type
@@ -45,68 +45,13 @@ for (let i = 0; i < arr_split.length; i++) {
         }
     }
 }
+    //third, delete element from array if contains a number greater than 14
+for (let i = arr_split.length - 1; i >= 0; i--) {
+    let arr_value = arr_split[i].value;
+
+    if (arr_value.some((element) => element > 14)) {
+      arr_split.splice(i, 1);
+    }
+  }
 console.log(arr_split);
 
-
-// let sum = 0;
-// for (let i = 0; i < text_by_line.length; i++) {
-//     let line = text_by_line[i];
-//     line = convert_to_number(line);
-//     combined_num = first_last_numbers(line);
-//     sum += combined_num;
-//     console.log(sum);
-// }
-
-// function first_last_numbers(line) {
-//     let first = true;
-//     let first_digit = 0;
-//     let last_digit = 0; 
-//     for (let i = 0; i < line.length; i++) {
-//         let char = line[i];
-//         if (char >= '0' && char <= '9') {
-//             if (first) {
-//                 first = false;
-//                 first_digit = char;
-//             }
-//             last_digit = char;
-//         }
-//     }
-//     let both_digits = first_digit + last_digit;
-//     new_number = Number(both_digits);
-
-//     return new_number;
-// }
-
-// function convert_to_number(line) {
-//     let number_word_map =
-//         {
-//             "one" : 1, 
-//             "two" : 2,
-//             "three" : 3, 
-//             "four" : 4, 
-//             "five" : 5, 
-//             "six" : 6, 
-//             "seven" : 7,
-//             "eight" : 8, 
-//             "nine" : 9
-//         };
-
-//     String.prototype.insertCharacter = function(index, replacement) {
-//         return this.substring(0, index) + replacement + this.substring(index + replacement.length-1); 
-//     }
-
-//     number_words = Object.keys(number_word_map);
-//     let i = 0;
-//     while (i < number_words.length) {
-//         let key = number_words[i];
-//         let index_in_string = line.indexOf(key);
-//         while (index_in_string != -1) {
-//             line = line.insertCharacter(index_in_string+(key.length-1), number_word_map[key].toString());
-//             console.log(line); 
-//             index_in_string = line.indexOf(key);
-//         }
-//         i++;
-//     }
-
-//     return line;
-// }
